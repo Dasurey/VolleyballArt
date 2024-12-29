efc0d873dfe5e57b689ab34173ae8c740cb99367
Dasurey <dario.asurey@gmail.com>
2024-12-29 04:18:21 -0300

    Terminado

--- js/script.js
+++ js/script.js
@@ -383,7 +383,8 @@ document.addEventListener('DOMContentLoaded', () => {
                                         <input type="email" class="form-control" id="email">
                                     </div>
                                     <div class="form-group mb-0">
-                                        <input type="" value="${generalData.review_product.form.submit}" class="btn btn-primary px-3">
+                                        <div class="btn btn-primary px-3">${generalData.review_product.form.submit}</div>
+                                        <!-- <input type="submit" value="${generalData.review_product.form.submit}" class="btn btn-primary px-3"> -->
                                     </div>
                                 </form>
                             </div>
